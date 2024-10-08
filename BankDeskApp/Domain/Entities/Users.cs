6ba706683b6d17997be3e97602e328524afd98cc
Anton Panychev <anton.panychev@ctechnology.kg>
2024-10-08 11:09:20 +0600

    init commit

---
+++ BankDeskApp/Domain/Entities/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDeskApp.Domain.Entities
+{
+    class Users
+    {
+        int id;
+        String username;
+        String salt;
+        String hashedPassword;
+    }
+}
